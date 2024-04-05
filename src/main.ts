6f7c439ae044ff396ebf39eb4c564b768d026df6
Artiom Sopin <artiomsopin@gmail.com>
2024-04-05 11:40:03 +0300

    feat: add crypto-js

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix("v1/api");
+  app.setGlobalPrefix("api/v1");
   await app.listen(process.env.APP_PORT || 8080, 
   () => console.log(`Server started on port ${process.env.APP_PORT}`));
 };
